3ad07394e7f81bb0bef59998a2b084fb14a0444b
nurulmn <nmasyanabila@gmail.com>
2025-05-25 14:59:49 +0700

    melihat detail film dengan fitur see detail menggunakan modal

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -22,7 +22,7 @@ function searchMovie(){
                                 <div class="card-body">
                                     <h5 class="card-title">`+ data.Title +`</h5>
                                     <h6 class="card-subtitle mb-2 text-body-secondary">`+ data.Year +`</h6>
-                                    <a href="#" class="card-link">See Detail</a>
+                                    <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="`+ data.imdbID+`">See Detail</a>
                                 </div>
                             </div>
                         </div>
@@ -55,3 +55,42 @@ $('#search-input').on('keyup', function (e) {
         searchMovie();
     }
 });
+
+$('#movie-list').on('click','.see-detail', function (){
+
+    $.ajax({
+        url:'http://omdbapi.com',
+        dataType:'json',
+        type: 'get',
+        data: {
+            'apikey' : '9794d919',
+            'i' : $(this).data('id')
+        },
+        success: function (movie) {
+            if(movie.Response === "True"){
+
+                $('.modal-body').html(`
+                <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-4">
+                                <img src="`+ movie.Poster+`" class="img-fluid">
+                            </div>
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                <li class="list-group-item"><h3>`+ movie.Title+`</h3></li>
+                                <li class="list-group-item">Released:`+ movie.Released+`</li>
+                                 <li class="list-group-item">Genre:`+ movie.Genre+`</li>
+                                 <li class="list-group-item">Director:`+ movie.Director+`</li>
+                                 <li class="list-group-item">Actor:`+ movie.Actors+`</li>
+                               
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                `)
+            }
+        }
+    })
+
+})
